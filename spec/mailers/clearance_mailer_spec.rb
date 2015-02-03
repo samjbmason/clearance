7489b366d98c74ccc0e30dbb4d0ee9ea748cc6c3
Melissa Xie <melissa@thoughtbot.com>
2015-02-03 16:03:46 -0500

    fix mailer specs

--- spec/mailers/clearance_mailer_spec.rb
+++ spec/mailers/clearance_mailer_spec.rb
@@ -2,49 +2,45 @@ require "spec_helper"
 
 describe ClearanceMailer do
   it "is from DO_NOT_REPLY" do
-    user = create(:user)
-    user.forgot_password!
+    password_reset = create(:password_reset)
 
-    email = ClearanceMailer.change_password(user)
+    email = ClearanceMailer.change_password(password_reset)
 
     expect(Clearance.configuration.mailer_sender).to eq(email.from[0])
   end
 
   it "is sent to user" do
-    user = create(:user)
-    user.forgot_password!
+    password_reset = create(:password_reset)
 
-    email = ClearanceMailer.change_password(user)
+    email = ClearanceMailer.change_password(password_reset)
 
-    expect(email.to.first).to eq(user.email)
+    expect(email.to.first).to eq(password_reset.user_email)
   end
 
   it "contains a link to edit the password" do
-    user = create(:user)
-    user.forgot_password!
-    host = ActionMailer::Base.default_url_options[:host]
-    link = "http://#{host}/users/#{user.id}/password/edit" \
-      "?token=#{user.confirmation_token}"
+    password_reset = create(:password_reset)
 
-    email = ClearanceMailer.change_password(user)
+    host = ActionMailer::Base.default_url_options[:host]
+    link = "http://#{host}/users/#{password_reset.user_id}/password/edit" \
+      "?token=#{password_reset.token}"
+
+    email = ClearanceMailer.change_password(password_reset)
 
     expect(email.body.to_s).to include(link)
   end
 
   it "sets its subject" do
-    user = create(:user)
-    user.forgot_password!
+    password_reset = create(:password_reset)
 
-    email = ClearanceMailer.change_password(user)
+    email = ClearanceMailer.change_password(password_reset)
 
     expect(email.subject).to include("Change your password")
   end
 
   it "contains opening text in the body" do
-    user = create(:user)
-    user.forgot_password!
+    password_reset = create(:password_reset)
 
-    email = ClearanceMailer.change_password(user)
+    email = ClearanceMailer.change_password(password_reset)
 
     expect(email.body).to include(
       I18n.t("clearance_mailer.change_password.opening")
@@ -52,10 +48,9 @@ describe ClearanceMailer do
   end
 
   it "contains closing text in the body" do
-    user = create(:user)
-    user.forgot_password!
+    password_reset = create(:password_reset)
 
-    email = ClearanceMailer.change_password(user)
+    email = ClearanceMailer.change_password(password_reset)
 
     expect(email.body.raw_source).to include(
       I18n.t("clearance_mailer.change_password.closing")
